aa04f97f82d5ee4e434253c875e7cca6ed541a33
Luke <contact@lukeolfert.me>
2017-04-04 18:44:38 -0500

    Version 1

--- app/src/main/java/com/example/luke/retag/activities/musicLoadingActivity.java
+++ app/src/main/java/com/example/luke/retag/activities/musicLoadingActivity.java
@@ -28,7 +28,7 @@ class getMusic extends AsyncTask<String, String, String> {
     @Override
     protected String doInBackground(String... Context) {
 
-        Context.
+        Context.get
         ContentResolver cr = getContentResolver();
         Songs songs = new Songs();
         songs.prepare();
